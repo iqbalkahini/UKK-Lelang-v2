21a3bc17405ffae92ecdfa8eae68f1ca58a9cfb7
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-01-24 13:06:18 +0700

    Update page-loading.tsx

--- components/page-loading.tsx
+++ components/page-loading.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import { Skeleton } from "@/components/ui/skeleton"
 import { SiteHeader } from "@/components/site-header"
 
 export function PageLoading() {
     return (
         <>
-            <SiteHeader />
+            <SiteHeader title={'Loading'} />
             <div className="flex flex-1 flex-col gap-4 p-4 md:gap-6 md:p-6">
                 <div className="space-y-2">
                     <Skeleton className="h-8 w-64" />
