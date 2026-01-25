12a95e953bf1783981fdbf6fa8a3a13eb0ad4776
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-01-25 12:15:59 +0700

    Update layout.tsx

--- app/petugas/barang/[id]/layout.tsx
+++ app/petugas/barang/[id]/layout.tsx
@@ -3,9 +3,9 @@ import { Suspense } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            {/* <Suspense fallback={<div>Loading...</div>}>
-            </Suspense> */}
-            {children}
+            <Suspense fallback={<div>Loading...</div>}>
+                {children}
+            </Suspense>
         </div>
     )
 }
