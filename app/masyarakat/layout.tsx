52f3470c0f01b728e112fbebf1843f0235237c17
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-03-21 22:21:35 +0700

    suspense

--- app/masyarakat/layout.tsx
+++ app/masyarakat/layout.tsx
@@ -1,5 +1,6 @@
 import { MasyarakatSidebar } from "@/components/masyarakat-sidebar"
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { Suspense } from "react"
 
 export default function MasyarakatLayout({
     children,
@@ -10,7 +11,9 @@ export default function MasyarakatLayout({
         <SidebarProvider>
             <MasyarakatSidebar />
             <SidebarInset>
-                {children}
+                <Suspense>
+                    {children}
+                </Suspense>
             </SidebarInset>
         </SidebarProvider>
     )
